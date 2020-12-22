53241ef00f43c17429bd320f60a36a2c9fd2229f
bmeznar <blaz.meznar.bm@gmail.com>
2020-12-22 14:08:01 +0100

    connecting videos and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_122529) do
+ActiveRecord::Schema.define(version: 2020_12_22_125635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,10 @@ ActiveRecord::Schema.define(version: 2020_12_22_122529) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "video"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "videos", "users"
 end
