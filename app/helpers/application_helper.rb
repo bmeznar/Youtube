fb1e6e34cceaaa6092734490843c0bdd53e1ddff
bmeznar <blaz.meznar.bm@gmail.com>
2020-12-22 14:35:03 +0100

    transfering navbar to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def display_main_navbar_options
+    capture do
+      if user_signed_in?
+        concat link_to "Odjava", destroy_user_session_path, method: :delete
+      else
+        concat link_to "Prijava", new_user_session_path
+        concat " "
+        concat link_to "Registracija", new_user_registration_path
+      end
+    end
+  end
 end
