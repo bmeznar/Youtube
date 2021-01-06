20f44ebdbaf574b8cdca096c1db979f2922892f9
bmeznar <blaz.meznar.bm@gmail.com>
2021-01-06 12:11:13 +0100

    simple navbar design+index design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,15 @@ module ApplicationHelper
   def display_main_navbar_options
     capture do
       if user_signed_in?
-        concat link_to "Odjava", destroy_user_session_path, method: :delete
+        concat link_to 'Naloži video', new_video_path, :class=>'btn btn-outline-secondary'
+        concat "    "
+        #concat link_to 'Uredi profil', edit_user_registration, :class=>'btn btn-outline-secondary'
+        concat "    "
+        concat link_to "Odjava", destroy_user_session_path, method: :delete, :class=>'btn btn-outline-secondary'
       else
-        concat link_to "Prijava", new_user_session_path
-        concat " "
-        concat link_to "Registracija", new_user_registration_path
+        concat link_to "Prijava", new_user_session_path, :class=>'btn btn-outline-secondary'
+        concat "    "
+        concat link_to "Registracija", new_user_registration_path, :class=>'btn btn-outline-secondary'
       end
     end
   end
