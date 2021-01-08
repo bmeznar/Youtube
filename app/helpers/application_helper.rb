4102396729ef806d09a721fa084b75318efd2272
bmeznar <blaz.meznar.bm@gmail.com>
2021-01-08 18:10:24 +0100

    putting like/dislike into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,28 @@ module ApplicationHelper
       end
     end
   end
+
+  def like_dislike
+    capture do
+      if user_signed_in?
+        if current_user.voted_up_on? @video
+          concat link_to "👍 #{@video.get_likes.size}", like_video_path(@video), :class=>"btn btn-secondary", method: :put
+        else
+          concat link_to like_video_path(@video), :class=>"btn btn-outline-secondary", method: :put do
+            concat "👍 "
+            concat @video.get_likes.size
+          end
+        end
+        if current_user.voted_down_on? @video
+          concat link_to "👍 #{@video.get_likes.size}", dislike_video_path(@video), :class=>"btn btn-secondary", method: :put
+        else
+          concat link_to "👎 #{@video.get_dislikes.size}", dislike_video_path(@video), :class=>"btn btn-outline-secondary", method: :put
+        end
+      else
+        concat link_to "👍 #{@video.get_likes.size}",  new_user_session_path, :class=>"btn btn-outline-secondary"
+        concat link_to "👎 #{@video.get_dislikes.size}", new_user_session_path, :class=>"btn btn-outline-secondary"
+      end
+    end
+  end
+
 end
