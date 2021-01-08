48e65a0a1130041b55a7fbf3768beb87827f9c7b
bmeznar <blaz.meznar.bm@gmail.com>
2021-01-08 19:58:50 +0100

    adding fropdown menu for login,register,....

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,15 @@ module ApplicationHelper
   def display_main_navbar_options
     capture do
       if user_signed_in?
-        concat link_to 'Naloži video', new_video_path, :class=>'btn btn-outline-secondary'
+        concat link_to 'Naloži video', new_video_path, :class=>'dropdown-item'
         concat "    "
-        #concat link_to 'Uredi profil', edit_user_registration, :class=>'btn btn-outline-secondary'
+        #concat link_to 'Uredi profil', edit_user_registration, :class=>'dropdown-item'
         concat "    "
-        concat link_to "Odjava", destroy_user_session_path, method: :delete, :class=>'btn btn-outline-secondary'
+        concat link_to "Odjava", destroy_user_session_path, method: :delete, :class=>'dropdown-item'
       else
-        concat link_to "Prijava", new_user_session_path, :class=>'btn btn-outline-secondary'
+        concat link_to "Prijava", new_user_session_path, :class=>'dropdown-item'
         concat "    "
-        concat link_to "Registracija", new_user_registration_path, :class=>'btn btn-outline-secondary'
+        concat link_to "Registracija", new_user_registration_path, :class=>'dropdown-item'
       end
     end
   end
