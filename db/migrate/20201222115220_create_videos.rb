436290c8a654f684d11ae6cd0d4371bf108b5e46
bmeznar <blaz.meznar.bm@gmail.com>
2021-01-26 12:24:23 +0100

    migration fix

--- db/migrate/20201222115220_create_videos.rb
+++ db/migrate/20201222115220_create_videos.rb
@@ -1,6 +1,6 @@
 class CreateVideos < ActiveRecord::Migration[6.0]
   def change
-    change_table :videos do |t|
+    create_table :videos do |t|
       t.string :title
       t.string :video
       t.text :description
