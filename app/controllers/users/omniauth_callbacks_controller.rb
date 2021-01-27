10cf3e98016f3de7f16da3417dc97805da94ca72
bmeznar <blaz.meznar.bm@gmail.com>
2021-01-27 10:48:41 +0100

    fb,google fix pt.9

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,7 +13,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
      @user = User.from_omniauth(request.env['omniauth.auth'])
-     @user.to_yaml
+     #puts @user.to_yaml
      if @user.persisted?
        sign_in_and_redirect @user
        set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
